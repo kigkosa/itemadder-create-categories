1ab471e086faad27b8d76f37a3a805abf081a6b5
phisut kosayamat <dady_ocat@hotmail.com>
2024-07-27 04:02:36 +0700

    f

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import re
 import yaml
 
-with open(r'epicrankicon_item.yml') as file:
+with open(r'animated_emoji_items.yml') as file:
     documents = yaml.full_load(file)
     data = {}
     data["info"] = documents["info"]
@@ -8,12 +9,13 @@ with open(r'epicrankicon_item.yml') as file:
     namespace  = data["info"]["namespace"]
     data["categories"][namespace] = {}
     items = []
-
+    # for item, doc in sorted(documents["items"],key=lambda x: int(x.split("_")[-1])):
     for item, doc in documents["items"].items():
         items.append(namespace+":"+item)
-    data["categories"][namespace]["name"] = namespace.replace("_"," ")
+    # items = sorted(items,key=lambda x: int(x.split("_")[-1]))
+    data["categories"][namespace]["name"] = namespace.replace("_"," ").title()
     data["categories"][namespace]["enabled"] = True
-    data["categories"][namespace]["icon"] = item
+    data["categories"][namespace]["icon"] = namespace+":"+item
     data["categories"][namespace]["permission"] = "ia.menu."+namespace
     data["categories"][namespace]["items"] = items
 
